d30e2ea686c9e5c2a68e57d3f79af536dca1b974
illuminosu <156883938+illuminosu@users.noreply.github.com>
2024-06-27 22:12:04 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2453,7 +2453,7 @@ addLayer("sb", {
         symbol: "SB", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         color: "#504899",
-        requires: new Decimal(100), // Can be a function that takes requirement increases into account
+        requires: new Decimal(1), // Can be a function that takes requirement increases into account
         resource: "super boosters", // Name of prestige currency
         baseResource: "boosters", // Name of resource prestige is based on
         baseAmount() {return player.b.points}, // Get the current amount of baseResource
@@ -2463,7 +2463,7 @@ addLayer("sb", {
         exponent() { return ((Array.isArray(tmp.ma.mastered))?tmp.ma.mastered.includes(this.layer):false)?1.075:1.25 }, // Prestige currency exponent
 		base() { return ((Array.isArray(tmp.ma.mastered))?tmp.ma.mastered.includes(this.layer):false)?1.025:1.05 },
 		gainMult() { 
-			let mult = new Decimal(1);
+			let mult = new Decimal(100);
 			if (hasUpgrade("ss", 21)) mult = mult.div(1.2);
 			if ((Array.isArray(tmp.ma.mastered))?tmp.ma.mastered.includes(this.layer):false) mult = mult.div(4/3);
 			return mult;
